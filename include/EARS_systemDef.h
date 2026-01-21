f9c8b7131f204960ec561057aa10cf4f1340bdc5
Julian Bourne <julian@britesc.com>
2026-01-21 19:37:47 +0100

    Building System Definies, Currently for NVS

--- include/EARS_systemDef.h
+++ include/EARS_systemDef.h
@@ -3,7 +3,7 @@
  * @author Julian (51fiftyone51fiftyone@gmail.com)
  * @brief EARS Project System Header File.
  * @version 0.1.0
- * @date 20260119
+ * @date 20260121
  * 
  * 
  */
@@ -11,10 +11,23 @@
 #ifndef __EARS_SYSTEM_DEF_H__
 #define __EARS_SYSTEM_DEF_H__
 
+/**********************************************************************
+ * System Definitions
+ **********************************************************************/
+
 #define EARS_SYSTEM_NAME        "EARS"
-#define EARS_NAMESPACE          "EARS"
 #define EARS_DESCRIPTION        "Equipment & Ammunition Reporting System"
 #define EARS_STATUS             "Development"
 #define EARS_COPYRIGHT          "Copyright JTB 2026 All Rights Reserved"
+
+/**********************************************************************
+ * NVS Definitions
+ **********************************************************************/
+#define EARS_NAMESPACE          "EARS"      // NVS Namespace
+#define EARS_VERSION_CODE       "EARS_VC"   // Version Code (2 Hexadecimal Characters (0-9, A-F to Represent Version of NVS Data)
+#define EARS_ZAPCODE            "EARS_ZC"   // 6 Character Code to Marry the Device to a User. It is 2 Uppercase Letters Followed by 4 Digits.
+#define EARS_PASSWORD_HASH      "EARS_PW"   // A CRC32 hash of the Password created by the User.
+#define EARS_CRC32              "EARS_32"   // CRC32 of all NVS Data to Ensure Integrity
+
 
 #endif // __EARS_SYSTEM_DEF_H__
