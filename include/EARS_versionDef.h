f60ccfc39b7ba1201b860e0d6decd8869c86d419
Julian Bourne <julian@britesc.com>
2026-02-07 22:48:52 +0100

    Before Touch I2C Check

--- include/EARS_versionDef.h
+++ include/EARS_versionDef.h
@@ -17,9 +17,9 @@
 
 #define EARS_APP_VERSION_MAJOR "4"
 #define EARS_APP_VERSION_MINOR "7"
-#define EARS_APP_VERSION_PATCH "100"
+#define EARS_APP_VERSION_PATCH "101"
 
-#define EARS_APP_BUILD_TIMESTAMP 20260206115248
+#define EARS_APP_BUILD_TIMESTAMP 20260207224455
 
 /**
  * @brief Convert string to integer at compile time
