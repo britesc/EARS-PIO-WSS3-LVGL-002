7811d2d00fcb9bca8c900d33a74d9e7ed543eea6
Julian Bourne <julian@britesc.com>
2026-02-13 12:03:43 +0100

    Freeze before resolving animation error.

--- include/EARS_versionDef.h
+++ include/EARS_versionDef.h
@@ -17,9 +17,9 @@
 
 #define EARS_APP_VERSION_MAJOR "4"
 #define EARS_APP_VERSION_MINOR "8"
-#define EARS_APP_VERSION_PATCH "26"
+#define EARS_APP_VERSION_PATCH "27"
 
-#define EARS_APP_BUILD_TIMESTAMP 20260212221005
+#define EARS_APP_BUILD_TIMESTAMP 20260212222216
 
 /**
  * @brief Convert string to integer at compile time
